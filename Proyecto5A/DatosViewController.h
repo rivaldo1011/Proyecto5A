3f137beee7e083fb9af65b4423f448ba7c547ad7
Issac <mojarraasesina21@gmail.com>
2022-04-12 12:06:20 -0500

    editar, creasion
    1. se reparo el proyecto.
    2. se agrego el controlador para la pantalla donde se mostraran los datos.

---
+++ Proyecto5A/DatosViewController.h
@@ -0,0 +1,16 @@
+//
+//  DatosViewController.h
+//  Proyecto5A
+//
+//  Created by Mac09 on 12/04/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DatosViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Proyecto5A/DatosViewController.m
@@ -0,0 +1,31 @@
+//
+//  DatosViewController.m
+//  Proyecto5A
+//
+//  Created by Mac09 on 12/04/22.
+//
+
+#import "DatosViewController.h"
+
+@interface DatosViewController ()
+
+@end
+
+@implementation DatosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
